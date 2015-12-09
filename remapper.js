b2eafb3ed6253eccf13cfdcdc16113f027607523
Maik Riechert <maik.riechert@arcor.de>
2015-12-09 14:26:08 +0000

    readd package.json and use jsplumb 2 (1.x not on npm)
    This should make it easier to use with other dependency managers,
    including JSPM.

--- remapper.js
+++ remapper.js
@@ -1,7 +1,7 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['jsPlumb'], factory);
+        define(['jsplumb'], factory);
     } else {
         // Browser globals
         root.Remapper = factory(root.jsPlumb);
