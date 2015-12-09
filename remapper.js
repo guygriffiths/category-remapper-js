af69e1d24e5e27372ececb5cb7bfc4d8b57530c5
Maik Riechert <maik.riechert@arcor.de>
2015-12-09 16:57:20 +0000

    fix commonjs/node boilerplate
    jsPlumb uses strict CommonJS which means that it doesn't export the main
    class itself but an object with a key jsPlumb which points to that main
    class.

--- remapper.js
+++ remapper.js
@@ -6,7 +6,7 @@
         // Node. Does not work with strict CommonJS, but
         // only CommonJS-like environments that support module.exports,
         // like Node.
-        module.exports = factory(require('jsplumb'));
+        module.exports = factory(require('jsplumb').jsPlumb);
     } else {
         // Browser globals (root is window)
         root.Remapper = factory(root.jsPlumb);
